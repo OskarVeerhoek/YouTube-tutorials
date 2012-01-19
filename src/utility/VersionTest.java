684152bbe413f8f3acef08c05de03ce48df6cee9
Oskar <Oskar@192.168.1.128>
2012-01-19 16:41:01 +0100

    Added the utility package with a OpenGL version checker class

---
+++ src/utility/VersionTest.java
@@ -0,0 +1,16 @@
+package utility;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.GL11;
+
+/**
+ * Prints out your version of OpenGL
+ * @author Oskar
+ */
+public class VersionTest {
+    public static void main(String[] args) throws LWJGLException {
+        System.out.println("Your OpenGL version is " + GL11.glGetString(GL11.GL_VERSION));
+        Display.destroy();
+    }
+}
