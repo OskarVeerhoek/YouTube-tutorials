5928d92d9518ed2e00f39feab8895fcaae2ffad8
Oskar Veerhoek <thecodinguniverse@gmail.com>
2012-03-24 09:13:00 +0100

    Added episode 24

---
+++ src/episode_24/Face.java
@@ -0,0 +1,16 @@
+package episode_24;
+
+import org.lwjgl.util.vector.Vector3f;
+
+/**
+ *
+ * @author Oskar
+ */
+public class Face {
+    public Vector3f vertex = new Vector3f(); // three indices, not vertices or normals!
+    public Vector3f normal = new Vector3f();
+    public Face(Vector3f vertex, Vector3f normal) {
+        this.vertex = vertex;
+        this.normal = normal;
+    }
+}
