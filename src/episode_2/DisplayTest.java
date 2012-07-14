3dfa92029fe3066a312911dc911088aca2cc8d88
Oskar Veerhoek <thecodinguniverse@gmail.com>
2012-07-14 12:38:25 +0200

    Added comments to episode 2.

--- src/episode_2/DisplayTest.java
+++ src/episode_2/DisplayTest.java
@@ -12,19 +12,24 @@ import org.lwjgl.opengl.DisplayMode;
 public class DisplayTest {
     public static void main(String[] args) {
         try {
+            // Sets the width of the display to 640 and the height to 480
             Display.setDisplayMode(new DisplayMode(640, 480));
-            Display.setTitle("Episode 2 – Display Test");
+            // Sets the title of the display to "Episode 2 - Display"
+            Display.setTitle("Episode 2 – Display");
+            // Creates and shows the display
             Display.create();
         } catch (LWJGLException e) {
-            System.err.println("The display couldn't be initialized.");
+            e.printStackTrace();
             System.exit(1);
         }
-
+        // While we aren't pressing the red button on the display
         while (!Display.isCloseRequested()) {
+            // Update the contents of the display and check for input
             Display.update();
+            // Wait until we reach 60 frames-per-second
             Display.sync(60);
         }
-
+        // Destroy the display and render it invisible
         Display.destroy();
         System.exit(0);
     }
