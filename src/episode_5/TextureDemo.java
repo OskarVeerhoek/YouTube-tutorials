0d9640df3e5127fb9cb622d12f4578b837118d52
Oskar Veerhoek <thecodinguniverse@gmail.com>
2012-07-14 12:28:17 +0200

    Cleaned up episode 5.

--- src/episode_5/TextureDemo.java
+++ src/episode_5/TextureDemo.java
@@ -1,26 +1,26 @@
 package episode_5;
 
-import static org.lwjgl.opengl.GL11.*;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import org.lwjgl.opengl.*;
-import org.lwjgl.*;
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
 import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import static org.lwjgl.opengl.GL11.*;
+
 /**
  * Nothing special, just a texture.
- * @author Oskar
+ * @author Oskar Veerhoek
  */
 public class TextureDemo {
 
-    private Texture wood;
+    private static Texture wood;
 
-    public TextureDemo() {
+    public static void main(String args[]) {
         try {
             Display.setDisplayMode(new DisplayMode(640, 480));
             Display.setTitle("Texture Demo");
@@ -28,42 +28,35 @@ public class TextureDemo {
         } catch (LWJGLException e) {
             e.printStackTrace();
         }
-
-        wood = loadTexture("wood");
-
-        // Initialization code OpenGL
+        try {
+            // Load the wood texture from "res/wood.png"
+            wood =  TextureLoader.getTexture("PNG", new FileInputStream(new File("res/wood.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+            Display.destroy();
+            System.exit(1);
+        }
         glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
         glOrtho(0, 640, 480, 0, 1, -1);
         glMatrixMode(GL_MODELVIEW);
         glEnable(GL_TEXTURE_2D);
-
         while (!Display.isCloseRequested()) {
-            // Render
-
             glClear(GL_COLOR_BUFFER_BIT);
-
             wood.bind();
-
             glBegin(GL_TRIANGLES);
-            
             glTexCoord2f(1, 0);
             glVertex2i(450, 10);
             glTexCoord2f(0, 0);
             glVertex2i(10, 10);
             glTexCoord2f(0, 1);
             glVertex2i(10, 450);
-            
             glTexCoord2f(0, 1);
             glVertex2i(10, 450);
             glTexCoord2f(1, 1);
             glVertex2i(450, 450);
             glTexCoord2f(1, 0);
             glVertex2i(450, 10);
-            
             glEnd();
-            
-            
 //            glBegin(GL_QUADS);
 //            glTexCoord2f(0, 0);
 //            glVertex2i(400, 400); // Upper-left
@@ -74,29 +67,12 @@ public class TextureDemo {
 //            glTexCoord2f(0, 1);
 //            glVertex2i(400, 450); // Bottom-left
 //            glEnd();
-
             Display.update();
             Display.sync(60);
         }
-
+        // Release the resources of the wood texture
+        wood.release();
         Display.destroy();
-    }
-
-    private Texture loadTexture(String key) {
-        try {
-            return TextureLoader.getTexture("PNG", new FileInputStream(new File("res/" + key + ".png")));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        new TextureDemo();
+        System.exit(0);
     }
 }
