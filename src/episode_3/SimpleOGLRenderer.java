392490b25a2d60cbae0e4d75b2420929dc134eb7
Oskar Veerhoek <thecodinguniverse@gmail.com>
2012-07-14 12:35:50 +0200

    Moved the code from the constructor to the main method.

--- src/episode_3/SimpleOGLRenderer.java
+++ src/episode_3/SimpleOGLRenderer.java
@@ -13,7 +13,7 @@ import static org.lwjgl.opengl.GL11.*;
  */
 public class SimpleOGLRenderer {
 
-    public SimpleOGLRenderer() {
+    public static void main(String[] args) {
         try {
             Display.setDisplayMode(new DisplayMode(640, 480));
             Display.setTitle("Hello, LWJGL!");
@@ -21,41 +21,28 @@ public class SimpleOGLRenderer {
         } catch (LWJGLException e) {
             e.printStackTrace();
         }
-
-        // Initialization code OpenGL
         glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
         glOrtho(0, 640, 480, 0, 1, -1);
         glMatrixMode(GL_MODELVIEW);
-
         while (!Display.isCloseRequested()) {
-            // Render
-
             glClear(GL_COLOR_BUFFER_BIT);
-
+            // Start rendering rectangles
             glBegin(GL_QUADS);
             glVertex2i(400, 400); // Upper-left
             glVertex2i(450, 400); // Upper-right
             glVertex2i(450, 450); // Bottom-right
             glVertex2i(400, 450); // Bottom-left
+            // Stop rendering rectangles
             glEnd();
-
+            // Start rendering lines
             glBegin(GL_LINES);
             glVertex2i(100, 100);
             glVertex2i(200, 200);
+            // Stop rendering lines
             glEnd();
-
             Display.update();
             Display.sync(60);
         }
-
         Display.destroy();
-    }
-
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        new SimpleOGLRenderer();
     }
 }
