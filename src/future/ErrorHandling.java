eb7754f9209ee937196810a3f7e93dfc452b04f6
Oskar Veerhoek <support@thecodinguniverse.com>
2012-08-21 17:47:06 +0200

    added core lighting, added shadow mapping, added error handling

---
+++ src/future/ErrorHandling.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2012, Oskar Veerhoek
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those
+ * of the authors and should not be interpreted as representing official policies,
+ * either expressed or implied, of the FreeBSD Project.
+ */
+
+package future;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.util.glu.GLU.gluErrorString;
+
+/**
+ * OpenGL error handling using glGetError and gluErrorString
+ */
+public class ErrorHandling {
+
+    private static final String WINDOW_TITLE = "Error Handling";
+    private static final int[] WINDOW_DIMENSIONS = {640, 480};
+
+    private static void printErrors() {
+        String error = gluErrorString(glGetError());
+        do {
+            System.err.println(error);
+            error = gluErrorString(glGetError());
+        } while (!error.equals("No error"));
+    }
+
+    private static void createErrors() {
+        //glBegin(GL_LIGHTING);
+    }
+
+    private static void cleanUp(boolean asCrash) {
+        Display.destroy();
+        System.exit(asCrash ? 1 : 0);
+    }
+
+    private static void setUpDisplay() {
+        try {
+            Display.setDisplayMode(new DisplayMode(WINDOW_DIMENSIONS[0], WINDOW_DIMENSIONS[1]));
+            Display.setTitle(WINDOW_TITLE);
+            Display.create();
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+            cleanUp(true);
+        }
+    }
+
+    public static void main(String[] args) {
+        setUpDisplay();
+        createErrors();
+        printErrors();
+        cleanUp(false);
+    }
+}
