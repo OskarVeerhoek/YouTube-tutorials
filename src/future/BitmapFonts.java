264a030ebefdbceba11a0f82b705f9598d6af455
Oskar Veerhoek <support@thecodinguniverse.com>
2013-03-29 19:14:19 +0100

    added comments, changed order of methods

--- src/future/BitmapFonts.java
+++ src/future/BitmapFonts.java
@@ -44,120 +44,10 @@ import static org.lwjgl.opengl.GL11.*;
 
 public class BitmapFonts {
 
-    private static final String WINDOW_TITLE = "Bitmap Fonts";
-    private static final int[] WINDOW_DIMENSIONS = {640, 480};
-
+    /** The string that is rendered on-screen. */
     private static final StringBuilder renderString = new StringBuilder("Enter your text");
+    /** The texture object for the bitmap font. */
     private static int fontTexture;
-
-    private static void render() {
-        glClear(GL_COLOR_BUFFER_BIT);
-        renderString(renderString.toString(), -0.9f, 0, 0.3f, 0.225f);
-    }
-
-    private static void renderCharacter(char character, float x, float y, float sizeX, float sizeY) {
-        int asciiCode = (int) character;
-        final float cellSize = 1.0f / 16.0f;
-        float cellX = asciiCode % 16 * cellSize;
-        float cellY = asciiCode / 16 * cellSize;
-        glBegin(GL_QUADS);
-        glTexCoord2f(cellX, cellY + cellSize);
-        glVertex2f(x, y);
-        glTexCoord2f(cellX + cellSize, cellY + cellSize);
-        glVertex2f(x + sizeX / 2, y);
-        glTexCoord2f(cellX + cellSize, cellY);
-        glVertex2f(x + sizeX / 2, y + sizeY);
-        glTexCoord2f(cellX, cellY);
-        glVertex2f(x, y + sizeY);
-        glEnd();
-    }
-
-    private static void renderString(String string, float x, float y, float characterSizeX, float characterSizeY) {
-        glPushMatrix();
-        glTranslatef(x, y, 0);
-        glBegin(GL_QUADS);
-        for (int i = 0; i < string.length(); i++) {
-            int asciiCode = (int) string.charAt(i);
-            final float cellSize = 1.0f / 16.0f;
-            float cellX = asciiCode % 16 * cellSize;
-            float cellY = asciiCode / 16 * cellSize;
-            glTexCoord2f(cellX, cellY + cellSize);
-            glVertex2f(i * characterSizeX / 3, y);
-            glTexCoord2f(cellX + cellSize, cellY + cellSize);
-            glVertex2f(i * characterSizeX / 3 + characterSizeX / 2, y);
-            glTexCoord2f(cellX + cellSize, cellY);
-            glVertex2f(i * characterSizeX / 3 + characterSizeX / 2, y + characterSizeY);
-            glTexCoord2f(cellX, cellY);
-            glVertex2f(i * characterSizeX / 3, y + characterSizeY);
-        }
-        glEnd();
-        glPopMatrix();
-    }
-
-    private static void input() {
-        while (Keyboard.next()) {
-            if (Keyboard.getEventKeyState()) {
-                if (Keyboard.getEventKey() != Keyboard.KEY_BACK) {
-                    if (Keyboard.getEventKey() != Keyboard.KEY_LSHIFT) {
-                        renderString.append(Keyboard.getEventCharacter());
-                    }
-                } else if (renderString.length() > 0) {
-                    renderString.setLength(renderString.length() - 1);
-                }
-            }
-        }
-    }
-
-    private static void cleanUp(boolean asCrash) {
-        glDeleteTextures(fontTexture);
-        Display.destroy();
-        System.exit(asCrash ? 1 : 0);
-    }
-
-    private static void setUpTextures() throws IOException {
-        fontTexture = glGenTextures();
-        glBindTexture(GL_TEXTURE_2D, fontTexture);
-        PNGDecoder decoder = new PNGDecoder(new FileInputStream("res/images/font.png"));
-        ByteBuffer buffer = BufferUtils.createByteBuffer(4 * decoder.getWidth() * decoder.getHeight());
-        decoder.decode(buffer, decoder.getWidth() * 4, PNGDecoder.Format.RGBA);
-        buffer.flip();
-        glBindTexture(GL_TEXTURE_2D, fontTexture);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, decoder.getWidth(), decoder.getHeight(), 0, GL_RGBA,
-                GL_UNSIGNED_BYTE, buffer);
-    }
-
-    private static void setUpStates() {
-        glEnable(GL_TEXTURE_2D);
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_ONE, GL_ONE);
-        glClearColor(0, 0.3f, 0, 1);
-    }
-
-    private static void update() {
-        Display.update();
-        Display.sync(60);
-    }
-
-    private static void enterGameLoop() {
-        while (!Display.isCloseRequested()) {
-            render();
-            input();
-            update();
-        }
-    }
-
-    private static void setUpDisplay() {
-        try {
-            Display.setDisplayMode(new DisplayMode(WINDOW_DIMENSIONS[0], WINDOW_DIMENSIONS[1]));
-            Display.setTitle(WINDOW_TITLE);
-            Display.create();
-        } catch (LWJGLException e) {
-            e.printStackTrace();
-            cleanUp(true);
-        }
-    }
 
     public static void main(String[] args) {
         setUpDisplay();
@@ -170,5 +60,124 @@ public class BitmapFonts {
         setUpStates();
         enterGameLoop();
         cleanUp(false);
+    }
+
+    private static void setUpDisplay() {
+        try {
+            Display.setDisplayMode(new DisplayMode(new int[]{640, 480}[0], new int[]{640, 480}[1]));
+            Display.setTitle("Bitmap Fonts");
+            Display.create();
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+            cleanUp(true);
+        }
+    }
+
+    private static void setUpTextures() throws IOException {
+        // Create a new texture for the bitmap font.
+        fontTexture = glGenTextures();
+        // Bind the texture object to the GL_TEXTURE_2D target, specifying that it will be a 2D texture.
+        glBindTexture(GL_TEXTURE_2D, fontTexture);
+        // Use TWL's utility classes to load the png file.
+        PNGDecoder decoder = new PNGDecoder(new FileInputStream("res/images/font.png"));
+        ByteBuffer buffer = BufferUtils.createByteBuffer(4 * decoder.getWidth() * decoder.getHeight());
+        decoder.decode(buffer, decoder.getWidth() * 4, PNGDecoder.Format.RGBA);
+        buffer.flip();
+        // Load the previously loaded texture data into the texture object.
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, decoder.getWidth(), decoder.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE,
+                buffer);
+    }
+
+    private static void setUpStates() {
+        glEnable(GL_TEXTURE_2D);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_ONE, GL_ONE);
+        glClearColor(0, 0.3f, 0, 1);
+    }
+
+    private static void enterGameLoop() {
+        while (!Display.isCloseRequested()) {
+            render();
+            input();
+            update();
+        }
+    }
+
+    private static void render() {
+        glClear(GL_COLOR_BUFFER_BIT);
+        renderString(renderString.toString(), -0.9f, 0, 0.3f, 0.225f);
+    }
+
+    private static void renderString(String string, float x, float y, float characterSizeX, float characterSizeY) {
+        glPushAttrib(GL_TEXTURE_BIT | GL_ENABLE_BIT);
+        // Enable linear texture filtering for smoothed results.
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        // Enable additive blending. This means that the colours will be added to already existing colours in the
+        // frame buffer. In practice, this makes the black parts of the texture become invisible.
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_ONE, GL_ONE);
+        // Store the current model-view matrix.
+        glPushMatrix();
+        // Offset all subsequent (at least up until 'glPopMatrix') vertex coordinates.
+        glTranslatef(x, y, 0);
+        glBegin(GL_QUADS);
+        // Iterate over all the characters in the string.
+        for (int i = 0; i < string.length(); i++) {
+            // Get the ASCII-code of the character by type-casting to integer.
+            int asciiCode = (int) string.charAt(i);
+            // There are 16 cells in a texture, and a texture coordinate ranges from 0.0 to 1.0.
+            final float cellSize = 1.0f / 16.0f;
+            // The cell's x-coordinate is the greatest integer smaller than remainder of the ASCII-code divided by the
+            // amount of cells on the x-axis, times the cell size.
+            float cellX = ((int) asciiCode % 16) * cellSize;
+            // The cell's y-coordinate is the greatest integer smaller than the ASCII-code divided by the amount of
+            // cells
+            // on the y-axis.
+            float cellY = ((int) asciiCode / 16) * cellSize;
+            glTexCoord2f(cellX, cellY + cellSize);
+            glVertex2f(i * characterSizeX / 3, y);
+            glTexCoord2f(cellX + cellSize, cellY + cellSize);
+            glVertex2f(i * characterSizeX / 3 + characterSizeX / 2, y);
+            glTexCoord2f(cellX + cellSize, cellY);
+            glVertex2f(i * characterSizeX / 3 + characterSizeX / 2, y + characterSizeY);
+            glTexCoord2f(cellX, cellY);
+            glVertex2f(i * characterSizeX / 3, y + characterSizeY);
+        }
+        glEnd();
+        glPopMatrix();
+        glPopAttrib();
+    }
+
+    private static void input() {
+        while (Keyboard.next()) {
+            if (Keyboard.getEventKeyState()) {
+                // Reset the string if we press escape.
+                if (Keyboard.getEventKey() == Keyboard.KEY_ESCAPE) {
+                    renderString.setLength(0);
+                }
+                // Append the pressed key to the string if the key isn't the back key or the shift key.
+                if (Keyboard.getEventKey() != Keyboard.KEY_BACK) {
+                    if (Keyboard.getEventKey() != Keyboard.KEY_LSHIFT) {
+                        renderString.append(Keyboard.getEventCharacter());
+                        //                        renderString.append((char) Keyboard.getEventCharacter() - 1);
+                    }
+                    // If the key is the back key, shorten the string by one character.
+                } else if (renderString.length() > 0) {
+                    renderString.setLength(renderString.length() - 1);
+                }
+            }
+        }
+    }
+
+    private static void update() {
+        Display.update();
+        Display.sync(60);
+    }
+
+    private static void cleanUp(boolean asCrash) {
+        glDeleteTextures(fontTexture);
+        Display.destroy();
+        System.exit(asCrash ? 1 : 0);
     }
 }
