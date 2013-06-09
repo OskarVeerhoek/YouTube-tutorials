e97e545fc146ebce92dfe2fed30b6269d39996b0
Oskar Veerhoek <support@thecodinguniverse.com>
2013-06-09 12:26:03 +0200

    updated gitignore, added vecmath library

---
+++ src/future/CoreDemo.java
@@ -0,0 +1,11 @@
+package future;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: TheCodingUniverse
+ * Date: 03/06/2013
+ * Time: 17:17
+ * To change this template use File | Settings | File Templates.
+ */
+public class CoreDemo {
+}
